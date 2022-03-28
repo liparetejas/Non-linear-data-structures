6f14d0fe335b57a6d03a58dd7eb99da66877da90
liparetejas <liparetejas13@gmail.com>
2022-03-29 00:14:09 +0530

    dfs added in traversal

--- Graphs/traversal.cpp
+++ Graphs/traversal.cpp
@@ -6,10 +6,10 @@
 using namespace std;
 
 // Time: O(n) Space: O(n)
-void bfs(ll n, vector<vector<ll>>& adjList) {
+void bfs(ll n, vector<vector<ll>> adjList) {
 	vector<bool> visited(n+1, false);
 	vector<ll> bfs_vect;
-	ll compenents = 0;
+	ll components = 0;
 	for(ll i = 1; i<=n; i++) {
 		if(!visited[i]) {
 			queue<ll> q;
@@ -27,14 +27,45 @@ void bfs(ll n, vector<vector<ll>>& adjList) {
 					}
 				}
 			}
-			compenents++;
+			components++;
 		}
 	}
-	cout << "No. of compenents in the graph: " << compenents << endl;
+	cout << "No. of components in the graph: " << components << endl;
 	cout << "BFS: ";
 	for(ll node : bfs_vect) {
 		cout << node << " ";
 	}
+	cout << endl;
+}
+
+
+// Time: O(n) Space: O(n)
+void dfs(ll n, vector<bool>& visited, vector<vector<ll>> adjList, vector<ll>& dfs_vect) {
+	dfs_vect.push_back(n);
+	visited[n] = true;
+	for(ll node : adjList[n]) {
+		if(!visited[node]) {
+			dfs(node, visited, adjList, dfs_vect);
+		}
+	}
+}
+
+void dfs_components(ll n, vector<vector<ll>> adjList) {
+	vector<bool> visited(n+1, false);
+	vector<ll> dfs_vect;
+	ll components = 0;
+	for(ll i=1; i<=n; i++) {
+		if(!visited[i]) {
+			dfs(i, visited, adjList, dfs_vect);
+			components++;
+		}
+	}
+	cout << "No. of components in the graph: " << components << endl;
+	cout << "DFS: ";
+	for(ll node : dfs_vect) {
+		cout << node << " ";
+	}
+	cout << endl;
 }
 
 int main()
@@ -62,8 +93,7 @@ int main()
 		cout << endl;
 	} */
 	bfs(n, adjList);
-	//dfs(n, adjList);
-
+	dfs_components(n, adjList);
 	return 0;	
 }
 
@@ -78,6 +108,6 @@ Input:
 5 7
 
 Output:
-No. of compenents in the graph: 2
+No. of components in the graph: 2
 BFS: 1 2 3 7 5 4 6
 */
